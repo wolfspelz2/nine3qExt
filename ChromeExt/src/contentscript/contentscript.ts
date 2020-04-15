8d56084fa30496a376d2f3de78e9e165fddd85ae
Heiner Wolf <git@heinerwolf.de>
2020-04-15 21:27:04 +0200

    content div with border

--- ChromeExt/src/contentscript/contentscript.ts
+++ ChromeExt/src/contentscript/contentscript.ts
@@ -1,4 +1,16 @@
-import './contentscript.scss';
+import "./contentscript.scss";
 
-const isThisContentscript = true;
-console.log('isThisContentscript', isThisContentscript);
+const isThisContentscript: boolean = true;
+console.log("isThisContentscript", isThisContentscript);
+
+var div = document.createElement("div");
+div.style.width = "100%";
+div.style.height = "100px";
+div.style.position = "absolute";
+div.style.left = "0px";
+div.style.bottom = "0px";
+div.style.backgroundColor = "transparent";
+div.style.border = "2px solid black";
+div.style.zIndex = "1000000000";
+div.innerHTML = "Hello World";
+document.body.appendChild(div); 
