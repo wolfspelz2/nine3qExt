60ee1329ceacace44ad4e61d9ad4891cbe1aff40
Heiner Wolf <git@heinerwolf.de>
2020-04-30 23:15:44 +0200

    even more final popup build env

--- ChromeExt/webpack.popup.config.js
+++ ChromeExt/webpack.popup.config.js
@@ -1,6 +1,9 @@
+const { CheckerPlugin } = require('awesome-typescript-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { optimize } = require('webpack');
 const { join } = require('path');
+let prodPlugins = [];
 
 if (process.env.NODE_ENV === 'production') {
     prodPlugins.push(
@@ -35,6 +38,8 @@ module.exports = {
         ]
     },
     plugins: [
+        new CheckerPlugin(),
+        ...prodPlugins,
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[name].css',
