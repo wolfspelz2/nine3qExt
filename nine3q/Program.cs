6ab5a04e5fd6cce3d471c875d91d0a2621f79cf2
Heiner Wolf <git@heinerwolf.de>
2020-05-03 15:45:57 +0200

    sln 4 devstudio

---
+++ nine3q/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nine3q
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
