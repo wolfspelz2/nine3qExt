cf187c728fab6ae437efb899a12387e17f450abc
Heiner Wolf <git@heinerwolf.de>
2020-05-07 19:42:27 +0200

    no config update (until later)

--- ChromeExt/src/background/ConfigUpdater.ts
+++ ChromeExt/src/background/ConfigUpdater.ts
@@ -5,29 +5,18 @@ import { Config } from '../lib/Config';
 
 export class ConfigUpdater
 {
-    private timer: number = undefined;
-
     async startUpdateTimer()
     {
-        this.timer = <number><unknown>setTimeout(async () =>
-        {
-            this.timer = undefined;
-            this.startUpdateTimer();
-            this.checkUpdate();
-        }, Config.get('config.checkUpdateIntervalSec', 600) * 1000);
     }
 
     stopUpdateTimer(): void
     {
-        if (this.timer != undefined) {
-            window.clearTimeout(this.timer);
-        }
     }
 
     async checkUpdate()
     {
         try {
-            let lastUpdateConfigTime: number = as.Int(await Config.getSync('config.lastUpdateTime', 0), 0);
+            let lastUpdateConfigTime: number = as.Int(Config.get('config.lastUpdateTime', 0), 0);
             if (Date.now() - lastUpdateConfigTime > as.Int(Config.get('config.updateIntervalSec', 86331))) {
                 await this.getUpdate()
             }
@@ -41,7 +30,7 @@ export class ConfigUpdater
         try {
             let data = await this.fetchConfig();
             Config.setAllOnline(data);
-            await Config.setSync('config.lastUpdateTime', Date.now());
+            Config.set('config.lastUpdateTime', Date.now());
         } catch (error) {
             log.info('ConfigUpdater.checkUpdate', 'fetchConfig failed')
         }
