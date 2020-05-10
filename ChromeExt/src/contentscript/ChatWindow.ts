9a4b20d2e25d2e67ac4f1dda367db184c6c904ff
Heiner Wolf <git@heinerwolf.de>
2020-05-10 21:59:07 +0200

    chatwin w/o jquery ui

--- ChromeExt/src/contentscript/ChatWindow.ts
+++ ChromeExt/src/contentscript/ChatWindow.ts
@@ -1,13 +1,12 @@
 import * as $ from 'jquery';
 import 'webpack-jquery-ui';
-import 'webpack-jquery-ui/css';
-import 'webpack-jquery-ui/dialog';
 // import markdown = require('markdown');
 import { as } from '../lib/as';
 import { Config } from '../lib/Config';
 import { Environment } from '../lib/Environment';
 import { ContentApp } from './ContentApp';
 import { Room } from './Room';
+import { Utils } from '../lib/Utils';
 
 class ChatLine
 {
@@ -32,26 +31,32 @@ export class ChatWindow
             this.addLine('1', 'Nickname', 'Lorem');
             this.addLine('2', 'ThisIsALongerNickname', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
             this.addLine('3', 'Long name with intmediate spaces', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum');
-        }
+            this.addLine('4', 'Long text no spaces', 'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm');
+       }
     }
 
     show()
     {
         if (!this.windowElem) {
-            let window = <HTMLElement>$('<div class="n3q-base n3q-window n3q-chatwindow n3q-shadow-medium" data-translate="children" />').get(0);
+            let windowId = Utils.randomString(10);
+
+            let window = <HTMLElement>$('<div id="' + windowId + '" class="n3q-base n3q-window n3q-chatwindow n3q-shadow-medium" data-translate="children" />').get(0);
             let titleBar = <HTMLElement>$('<div class="n3q-base n3q-window-title-bar" data-translate="children" />').get(0);
-            let title = <HTMLElement>$('<div class="n3q-base n3q-window-title" data-translate="text:Chatwindow">Chat History</div>').get(0);
+            let title = <HTMLElement>$('<div class="n3q-base n3q-window-title" data-translate="children" />').get(0);
+            let titleText = <HTMLElement>$('<div class="n3q-base n3q-window-title-text" data-translate="text:Chatwindow">Chat History</div>').get(0);
             let close = <HTMLElement>$(
                 `<div class="n3q-base n3q-window-button" title="Close" data-translate="attr:title:Common">
                     <div class="n3q-base n3q-button-symbol n3q-button-close" />
                 </div>`
             ).get(0);
             let content = <HTMLElement>$('<div class="n3q-base n3q-window-content" data-translate="children" />').get(0);
+            let resize = <HTMLElement>$('<div class="n3q-base n3q-window-resize n3q-window-resize-se"/>').get(0);
             let chatout = <HTMLElement>$('<div class="n3q-base n3q-chatwindow-chatout" data-translate="children" />').get(0);
             let chatin = <HTMLElement>$('<div class="n3q-base n3q-chatwindow-chatin" data-translate="children" />').get(0);
             let chatinText = <HTMLElement>$('<textarea class="n3q-base n3q-chatwindow-chatin-input n3q-input n3q-text" rows="1" placeholder="Enter chat here..." data-translate="attr:placeholder:Chatin" />').get(0);
             let chatinSend = <HTMLElement>$('<div class="n3q-base n3q-button n3q-button-inline" title="SendChat" data-translate="attr:title:Chatin"><div class="n3q-base n3q-button-symbol n3q-button-sendchat" />').get(0);
 
+            $(title).append(titleText);
             $(titleBar).append(title);
             $(titleBar).append(close);
             $(window).append(titleBar);
@@ -62,18 +67,32 @@ export class ChatWindow
             $(content).append(chatout);
             $(content).append(chatin);
 
+            $(window).append(resize);
             $(window).append(content);
 
             this.app.translateElem(window);
 
+            this.chatinInputElem = chatinText;
+            this.chatoutElem = chatout;
+            this.windowElem = window;
+
+            $(this.display).append(window);
+
+            $(window).resizable({
+                handles: {
+                    'se': '#n3q #' + windowId + ' .n3q-window-resize-se',
+                }
+            });
+
             $(chatinText).on('keydown', ev =>
             {
-                this.onKeydown(ev);
+                return this.onChatinKeydown(ev);
             });
 
             $(chatinSend).click(ev =>
             {
                 this.sendChat();
+                ev.stopPropagation();
             });
 
             $(close).click(ev =>
@@ -82,6 +101,7 @@ export class ChatWindow
             });
 
             $(window).draggable({
+                handle: '.n3q-window-title',
                 scroll: false,
                 stack: '.n3q-entity',
                 // opacity: 0.5,
@@ -89,16 +109,10 @@ export class ChatWindow
                 containment: 'document',
             });
 
-            this.chatinInputElem = chatinText;
-            this.chatoutElem = chatout;
-            this.windowElem = window;
-
             for (let id in this.lines) {
                 let line = this.lines[id];
                 this.showLine(line.nick, line.text);
             }
-
-            $(this.display).append(window);
         }
 
         this.pushToTop();
@@ -128,12 +142,12 @@ export class ChatWindow
             <div>`
         ).get(0);
 
-        if (this.chatoutElem != null) {
+        if (this.chatoutElem) {
             $(this.chatoutElem).append(lineElem).scrollTop($(this.chatoutElem).get(0).scrollHeight);
         }
     }
 
-    private onKeydown(ev: JQuery.Event)
+    private onChatinKeydown(ev: JQuery.Event): boolean
     {
         var keycode = (ev.keyCode ? ev.keyCode : (ev.which ? ev.which : ev.charCode));
         switch (keycode) {
@@ -149,7 +163,7 @@ export class ChatWindow
         }
     }
 
-    private sendChat()
+    private sendChat(): void
     {
         var text: string = as.String($(this.chatinInputElem).val(), '');
         if (text != '') {
@@ -158,27 +172,11 @@ export class ChatWindow
         }
     }
 
-    private onChatinKeydown(ev: JQuery.Event)
-    {
-        var keycode = (ev.keyCode ? ev.keyCode : (ev.which ? ev.which : ev.charCode));
-        switch (keycode) {
-            case 13:
-                this.sendChat();
-                return false;
-            case 27:
-                this.close();
-                ev.stopPropagation();
-                return false;
-            default:
-                return true;
-        }
-    }
-
     private pushToTop()
     {
     }
 
-    private close()
+    private close(): void
     {
         $(this.windowElem).remove();
         this.windowElem = null;
