282a6d5fb6a2ea905cc19603e5819e90308a0a23
Heiner Wolf <git@heinerwolf.de>
2020-05-17 10:57:25 +0200

    use netstandard 2.1 4 libs

---
+++ Backend/src/GrainInterfaces/IInventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Orleans;
+using nine3q.Items;
+
+namespace GrainInterfaces
+{
+    public interface IInventory : IGrainWithStringKey
+    {
+        Task<ItemId> CreateItem(PropertySet properties);
+        Task<bool> DeleteItem(ItemId id);
+    }
+}
