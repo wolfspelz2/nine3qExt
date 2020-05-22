cb6a6b3b827d9abc93f305d8c3071a15ad219cc3
Heiner Wolf <git@heinerwolf.de>
2020-05-22 21:59:02 +0200

    add frontend proj 2 backend, https://nine3q.dev.sui.li/images/Items/

--- Backend/nine3q.Frontend/PropertyFilter.cs
+++ Backend/nine3q.Frontend/PropertyFilter.cs
@@ -5,7 +5,9 @@
         public static string Url(string url)
         {
             return url
-                .Replace("{item.nine3q}", "http://localhost:5000/images/Items/")
+                .Replace("{item.nine3q}", "https://nine3q.dev.sui.li/images/Items/")
+                //.Replace("{item.nine3q}", "http://localhost:5000/images/Items/")
+
                 .Replace("{avatar.zweitgeist}", "http://avatar.zweitgeist.com/")
                 ;
         }
