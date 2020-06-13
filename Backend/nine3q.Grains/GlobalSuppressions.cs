2c0705817f3fffd53b82f915ba42d0a2fe8a9c0c
Heiner Wolf <git@heinerwolf.de>
2020-06-14 00:58:28 +0200

    finally removed Backend folder

--- Backend/nine3q.Grains/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "<Pending>", Scope = "member", Target = "~F:nine3q.Grains.CachedStringState.CachedString.Data")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "<Pending>", Scope = "member", Target = "~F:nine3q.Grains.InventoryState.Id")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "<Pending>", Scope = "member", Target = "~F:nine3q.Grains.TestStringState.TestString.Data")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "<Pending>", Scope = "member", Target = "~F:nine3q.Grains.InventoryState.StreamNamespace")]
