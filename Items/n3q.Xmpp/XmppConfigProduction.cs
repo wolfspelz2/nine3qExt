88072553a2f91fbddebc39f462d6c5ba1ff991a6
Heiner Wolf <git@heinerwolf.de>
2020-06-26 20:16:31 +0200

    xmpp: local config

---
+++ Items/n3q.Xmpp/XmppConfigProduction.cs
@@ -0,0 +1,11 @@
+﻿namespace n3q.Xmpp
+{
+    class XmppConfigProduction : XmppConfig
+    {
+        public void Load()
+        {
+            ConfigSequence += " XmppConfigProduction";
+            ComponentSecret = "Jn3Gd9R5r6hgFGhu5drvU1bh";
+        }
+    }
+}
