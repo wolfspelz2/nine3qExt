f088092eabbfde33ef0fbc2e182e34488973ef2b
Heiner Wolf <git@heinerwolf.de>
2021-01-27 12:47:48 +0100

    start ver 1.0.7

--- ChromeExt/src/lib/_Changes.ts
+++ ChromeExt/src/lib/_Changes.ts
@@ -6,6 +6,9 @@ interface IHistory extends Array<IRelease> { }
 export class _Changes
 {
     static data: IHistory = [
+        ['1.0.7', 'Items', [
+            ['Add', 'Have items'],
+        ]],
         ['1.0.6', 'PrivateChat Greet RecvDependentItems', [
             ['Add', 'Allow vidconf fullscreen'],
             ['Add', 'Private chat'],
