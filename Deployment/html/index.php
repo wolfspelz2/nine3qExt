c0bdf623fc918da10a7d2ebc91f08f516aa9ed20
Suat Özgür <s.oezguer@gmail.com>
2021-02-04 12:26:08 +0100

    get html meta tags from iframed page

---
+++ Deployment/html/index.php
@@ -0,0 +1,79 @@
+<?php
+
+ini_set('display_errors', 1);
+include(dirname(__FILE__) . '/functions.inc.php');
+$isDenied = false;
+$url = substr($_SERVER['REQUEST_URI'], 1);
+
+if (preg_match('/^http:\/\//si', $url)) {
+    $isDenied = true;
+    $isDeniedReason = 'https required';
+} elseif (!preg_match('/^https:\/\//si', $url)) {
+    $url = 'https://' . $url;
+}
+
+$r = getPage($url, 1);
+
+if (count($r['errors']) > 0) {
+    $isDenied = true;
+    $isDeniedReason = implode(', ', $r['errors']);
+} elseif ($r['header']['xhttp_code'] != 200) {
+    header($r['header']['xhttp_code'] . ' ' . $r['header']['xhttp_status'], true, $r['header']['xhttp_code']);
+    $isDenied = true;
+    $isDeniedReason = $r['header']['xhttp_code'] . ' ' . $r['header']['xhttp_status'];
+} elseif (isset($r['header']['content-security-policy']) && preg_match('/frame-ancestors/si', $r['header']['content-security-policy'])) {
+    $isDenied = true;
+    $isDeniedReason = 'iframe denied (1)';
+} elseif (isset($r['header']['x-frame-options']) && strtoupper($r['header']['x-frame-options']) == 'DENY') {
+    $isDenied = true;
+    $isDeniedReason = 'iframe denied (2)';
+}
+
+$htmlHead = getHtmlHead($r['body']);
+
+
+?><!DOCTYPE html>
+<html>
+<head>
+    <?= implode("\n    ", $htmlHead) ?>
+    <style type="text/css">
+        body {
+            margin: 0;
+        }
+
+        iframe {
+            display: block;
+            border: none;
+            height: 100vh;
+            width: 100vw;
+        }
+
+        .error {
+            margin: auto;
+            width: 50%;
+            border: 3px solid red;
+            padding: 10px;
+            margin-top: 200px;
+        }
+    </style>
+    <link href="v1/embedded.css" rel="stylesheet">
+</head>
+<body>
+<?php if ($isDenied) { ?>
+<div class="error">weblin is not available on this page (<?=htmlspecialchars($isDeniedReason)?>)</div>
+<?php } else { ?>
+<iframe id="iframe" src=""></iframe>
+<?php } ?>
+<script>
+    let url = '<?=$url?>';
+    document.getElementById('iframe').src = url;
+
+    var n3qConfig = {
+        apiVersion: '1',
+        pageUrl: url,
+        x: <?=rand(100, 400)?>
+    };
+</script>
+<script src="v1/embedded.js"></script>
+</body>
+</html>
