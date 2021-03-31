a62dd272e4b64a65195c2925fef6ebc971bdd1dd
Heiner Wolf <git@heinerwolf.de>
2021-03-31 20:22:56 +0200

    _comment

--- ChromeExt/src/lib/_Changes.ts
+++ ChromeExt/src/lib/_Changes.ts
@@ -10,7 +10,7 @@ export class _Changes
             ['Add', 'Scripting to iframe API & Autorez (Benji, Meerkat)'],
             ['Add', 'Clear chat window /clear'],
             ['Add', 'Inactive mode (xmpp: presence-show away) and multi-tab presence-show management'],
-            ['Change', 'Stay on page if item dropped (which makes Benji a bit unhappy/laggy)'],
+            ['Change', 'Stay on page if item dropped (which makes Benji a bit unwilling to follow when switching tabs)'],
             ['Change', 'Removed stay-on-tab-change menu entry (there are enough ways to stay)'],
             ['Change', 'Quick slide items without defined avatar speed'],
             ['Change', 'Nickname excluded from copy all text (not selectable)'],
