1506210c4df218f634b9b658ce4796d1db975e66
Heiner Wolf <git@heinerwolf.de>
2021-05-16 12:53:52 +0200

    vpAnimationsUrl AND AvatarUrl

--- ChromeExt/src/contentscript/Participant.ts
+++ ChromeExt/src/contentscript/Participant.ts
@@ -152,6 +152,7 @@ export class Participant extends Entity
                     vpAvatarId = as.String(attrs.AvatarId, '');
                     if (vpAvatarId == '') { vpAvatarId = as.String(attrs.avatar, ''); }
                     vpAnimationsUrl = as.String(attrs.AnimationsUrl, '');
+                    vpAnimationsUrl = as.String(attrs.AvatarUrl, vpAnimationsUrl);
                     vpImageUrl = as.String(attrs.ImageUrl, '');
                 }
             }
